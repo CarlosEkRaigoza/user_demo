3b5621251e385896dc02cc009e5452bd0ef3a09b
CarlosEkRaigoza <carlosroberto2624@gmail.com>
2025-12-09 09:44:00 -0600

    Configuración: Dockerización, conexión a MySQL y scripts SQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS usuario (
-    id IDENTITY PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255),
     num_cuenta BIGINT,
     num_telefono BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS direccion (
-    id IDENTITY PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     usuario_id BIGINT,
     calle VARCHAR(255),
     ciudad VARCHAR(255),
